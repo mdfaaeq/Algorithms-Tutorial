16c575917c28d494b8ff0e21ad26dd03b98659c9
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-30 17:13:27 +0530

    Coding while learning algorithms

---
+++ Learning Algorithms/Algorithm/src/main/java/com/learn/algorithm/Stack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.learn.algorithm;
+
+/**
+ *
+ * @author faaeq
+ */
+public class Stack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
